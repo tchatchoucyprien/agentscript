428ffac08bda894d4aaaf35a5268678d8d8dfb98
cyprien <tchatchou77@yahoo.fr>
2020-09-06 22:32:31 -0400

    Delete 14 days Old files_Final

--- delolderfile.sh
+++ delolderfile.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #Author: Cyprien
 #Date: September 2020
-#Description: The script below will chmod all file with .log extension older than 14 days
-
+#Description: The script below will delete all file with .log extension older than 14 days
 
 echo "Below are the files that are 14 days old or more on our server:"
 
+#find /var/log/ -name '*.log' -mtime +14 -exec rm -rf '{}' \;
 find /var/log/ -mtime +14 -name "*.log"
 
-echo " Do you want to delete them? Please press y for yes or n for no "
+function INPUT {
+echo "Do you want to delete them? Please press y for yes or n for no "
 
 read ANSWER
-
-
-function INPUT {
 
 case $ANSWER in
 
@@ -21,43 +19,19 @@ case $ANSWER in
 
 #       when ready, replace the chmod below by delete or rm -f
 
-        echo "$(find /var/log/ -mtime +14 -name "*.log" -exec chmod +x '{}' \;)Your files are now deleted."
+        echo "$(find /var/log/ -mtime +14 -name "*.log" -exec rm -rf '{}' \;)Your files are now deleted."
+        exit 0
         ;;
         n)
 
         echo "Those file that are 14 days old will not be deleted at the moment, Thank you."
+        exit 0
         ;;
         *)
-        echo "You did not answer by y or n, please re-enter your choice."
-        exit
+        echo "You did not answer by y or n, please re-enter your choice." && sleep 1 && INPUT
+        exit 0
         ;;
 esac
 }
-#while [ $ANSWER = y ] || [ $ANSWER = n ]
-
-
-#do
-
 INPUT
-
-#done
-
-#read ANSWER
-#INPUT
-
-#       echo "You did not answer by y or n, please re-enter your choice. y for yes or n for no"
-#read ANSWER
-
-#while [ $ANSWER != "y"  ] || [ $ANSWER != "n" ]
-
-#do
-#       echo " Please enter y for y or n for no"
-#read ANSWER
-
-#INPUT
-#done
-#exit
-#fi
-
-~
 
